6a4cdaa2f7d4a84291d7f28c01e16a2ca43fb0f2
Merdzhak <alex.merdzhanov@gmail.com>
2026-07-13 14:22:01 +0300

    fix: add findByEmail to UserRepository and implement SCRUM-12

--- safebanking/src/main/java/com/elsys/safebanking/repository/BankingTransactionRepository.java
+++ safebanking/src/main/java/com/elsys/safebanking/repository/BankingTransactionRepository.java
@@ -1,22 +1,23 @@
 package com.elsys.safebanking.repository;
 
 import com.elsys.safebanking.model.BankingTransaction;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.time.Instant;
-import java.util.List;
 
 public interface BankingTransactionRepository extends JpaRepository<BankingTransaction, Long> {
 
-    @Query("SELECT t FROM BankingTransaction t WHERE t.sourceAccount.owner.id = :userId OR t.destinationAccount.owner.id = :userId ORDER BY t.timeStamp DESC")
-    List<BankingTransaction> findAllUserTransactions(@Param("userId") Long userId);
+    @Query("SELECT t FROM BankingTransaction t WHERE t.sourceAccount.owner.id = :userId OR t.destinationAccount.owner.id = :userId")
+    Page<BankingTransaction> findAllUserTransactions(@Param("userId") Long userId, Pageable pageable);
 
-    @Query("SELECT t FROM BankingTransaction t WHERE (t.sourceAccount.owner.id = :userId OR t.destinationAccount.owner.id = :userId) AND t.timeStamp BETWEEN :startDate AND :endDate ORDER BY t.timeStamp DESC")
-    List<BankingTransaction> findUserTransactionsInDateRange(
+    @Query("SELECT t FROM BankingTransaction t WHERE (t.sourceAccount.owner.id = :userId OR t.destinationAccount.owner.id = :userId) AND t.timeStamp BETWEEN :startDate AND :endDate")
+    Page<BankingTransaction> findUserTransactionsInDateRange(
             @Param("userId") Long userId,
             @Param("startDate") Instant startDate,
-            @Param("endDate") Instant endDate
-    );
+            @Param("endDate") Instant endDate,
+            Pageable pageable);
 }
