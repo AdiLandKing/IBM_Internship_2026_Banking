ea71a1f8ae06ad4626550d12e1d661db7c4f990e
Kristiyan Kobarelov <123277206+KristiyanKobarelov@users.noreply.github.com>
2026-07-13 11:38:19 +0300

    fixed CI failure

--- safebanking/src/main/java/com/elsys/safebanking/repository/BankAccountRepository.java
+++ safebanking/src/main/java/com/elsys/safebanking/repository/BankAccountRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 import java.util.Optional;
 
-public interface BankAccountRepository extends JpaRepository<BankAccount, Integer> {
+public interface BankAccountRepository extends JpaRepository<BankAccount, String> {
     
     Optional<BankAccount> findByIban(String iban);
     long countByOwner(User owner);
